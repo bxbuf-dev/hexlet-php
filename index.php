f795ed8a4e5b88dbcba159b529d332059b973ef4
Aleksei Semenchenko <bxbuf-dev@outlook.com>
2021-03-29 14:51:47 +0200

    updates index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo 'Hello, Hexlet!';
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
